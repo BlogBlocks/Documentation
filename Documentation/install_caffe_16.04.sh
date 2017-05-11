db4356e589970574fb9d5c179786d3258aa5000e
Jack Northrup <jahral@yahoo.com>
2017-05-12 06:27:43 +0800

    Tidy up a bit

--- Documentation/install_caffe_16.04.sh
+++ Documentation/install_caffe_16.04.sh
@@ -3,37 +3,36 @@
 set -v
 NUMBER_OF_CORES=2
 
-apt-get -y update  
-apt-get -y install locate
-apt-get -y install make
-apt-get -y install nano
-apt-get -y install cmake
-apt-get -y install git  
-apt-get -y install curl  
-apt-get -y install gcc-4.6 build-essential  
-apt-get -y install python-dev  
-apt-get -y install python-pip  
-pip install --upgrade pip  
-pip install numpy  
-apt-get -y install liblmdb-dev  
-apt-get -y install libhdf5-serial-dev  
-apt-get -y install libleveldb-dev libsnappy-dev  
-apt-get -y install libopencv-dev  
-apt-get -y install libprotobuf-dev  
-apt-get -y install protobuf-compiler  
-apt-get -y install --no-install-recommends libboost-all-dev  
-apt-get -y install libgflags-dev  
-apt-get -y install libgoogle-glog-dev  
-apt-get -y install libatlas-base-dev  
+apt-get update  
+apt-get -y install locate \
+make \
+nano \
+cmake \
+git \
+curl \
+gcc-4.6 build-essential \
+python-dev \
+python-pip \
+liblmdb-dev \
+libhdf5-serial-dev \
+libleveldb-dev libsnappy-dev \
+libopencv-dev \
+libprotobuf-dev \
+protobuf-compiler \
+--no-install-recommends libboost-all-dev  
+libgflags-dev \
+libgoogle-glog-dev \
+libatlas-base-dev \
+pip install --upgrade pip \
+pip install numpy \
+pip install -U setuptools 
+
 git clone https://github.com/BVLC/caffe.git Caffe  
 cd Caffe  
-
-
-# Prepare Python binding (pycaffe)
 cd python
 for req in $(cat requirements.txt); do pip install $req; done
-echo "export PYTHONPATH=$(pwd):$PYTHONPATH " >> ~/.bash_profile # to be able to call "import caffe" from Python after reboot
-source ~/.bash_profile # Update shell 
+echo "export PYTHONPATH=$(pwd):$PYTHONPATH " >> ~/.bash_profile 
+source ~/.bash_profile 
 cd ..
 echo "INCLUDE_DIRS += /usr/include/hdf5/serial" >> Makefile.config
 echo "LIBRARY_DIRS += /usr/lib/x86_64-linux-gnu /usr/lib/x86_64-linux-gnu/hdf5/serial" >> Makefile.config
@@ -42,7 +41,13 @@ echo "PYTHON_INCLUDE := /usr/local/lib/python2.7/dist-packages/numpy/core/includ
 cp Makefile.config.example Makefile.config
 sed -i '8s/.*/CPU_ONLY := 1/' Makefile.config # Line 8: CPU only
 apt-get -y install -y libopenblas-dev
-sed -i '33s/.*/BLAS := open/' Makefile.config # Line 33: to use OpenBLAS
+sed -i '33s/.*/BLAS := open/' Makefile.config
+find . -type f -exec sed -i -e 's^"hdf5.h"^"hdf5/serial/hdf5.h"^g' -e 's^"hdf5_hl.h"^"hdf5/serial/hdf5_hl.h"^g' '{}' \;
+cd /usr/lib/x86_64-linux-gnu
+sudo ln -s libhdf5_serial.so.8.0.2 libhdf5.so
+sudo ln -s libhdf5_serial_hl.so.8.0.2 libhdf5_hl.so
+cd /Caffe
+ # Line 33: to use OpenBLAS
 # Note that if one day the Makefile.config changes and these line numbers change, we're screwed
 # Maybe it would be best to simply append those changes at the end of Makefile.config 
 echo "export OPENBLAS_NUM_THREADS=($NUMBER_OF_CORES)" >> ~/.bash_profile 
@@ -60,6 +65,7 @@ make test
 make runtest
 #make matcaffe
 make distribute
+mkdir /usr/local/caffe
 cp -ar /scripts/Caffe/distribute /usr/local/caffe  
 if [[ -z $(cat ~/.bashrc | grep /usr/local/caffe/bin) ]] ; then  
     echo -e "\n# Adds Caffe to the PATH variable" >> ~/.bashrc
